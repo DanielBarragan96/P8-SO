a4696bbfc8ae20f76fd69facb22ddf549eb166c7
DanielBarragan96 <ie702554@iteso.mx>
2018-05-07 12:37:28 -0500

    Update vdconfig.c

--- vdconfig.c
+++ vdconfig.c
@@ -45,11 +45,39 @@ int vdwrite(int fd, char *buffer, int bytes)
     currblock = *currptr;
     
     //Si el bloque está en blanco, dale uno
-    if(currblock == 0)
+    if(currblock == 0)//el bloque no existe
     {
      currblock = nextfreeblock();
-      //El bloque encontrdo ponerlo donde
+      //El bloque encontrdo ponerlo donde apunta el apuntador al bloque actual
+      *currptr = currblock;
+      assignblock(currblock);//Asignarlo en el mapa de bits
       
+      //Esccribir en el sector de la tabla de nodos i en el disco
+      sector = (currinode/8);
+      
+      result = vdwriteseclog(inicio_nodos_i + sector, &inode[sect8]);
+      
+      //Si bloquee de pos actual no está en mem, lee el buffer del archivo
+      if(openfiles[fd].currbloqueenmemoria!=currblock)
+      {
+       //Lee el bloque actual hasta el buffer que ***
+        ****open
+      }
+      //Copia el caracter al buffer
+      openfiles_[fd].buffer[openfiles[fd].curpos%TAMBLOQUE] = fer[cont];
+      //incrementa posición actual del actual
+      openfiles[fd].curpos++;
+      //Si la posición es mayor que el tamaño, modifica el tamaño
+      if(openfiles[fd].currpos > inode[currinode].size)
+      {
+       inode[openfiles[fd].inode].size = openfiles[fd].currpos;
+      }
+      cont++;
+      //Si se llena el buff, escribelo
+      if(openfiles[fd].currpos%TAMBLOQUE==0)
+        writeblock(currblok, openfiles[fd].buffer);
+      return(cont);
+      //TODO en vdclose hay que escribir el archivo **********
     }
   }
 }
