68746c3c1f17fc66dce5cf2917aa59e1609d4e2d
DanielBarragan96 <ie702554@iteso.mx>
2018-05-07 12:18:04 -0500

    more vdwrite

--- vdconfig.c
+++ vdconfig.c
@@ -33,7 +33,25 @@ int vdwrite(int fd, char *buffer, int bytes)
   
   currinode = openfiles[fd].inode;
   
-  //Copiar byte por byte 
+  //Copiar byte por byte del buffer que recibo com argumento al buffer del archivo
+  while(cont < bytes)
+  {
+    //Obtener la direción de donde está el bloque que corresponde a la posición actual
+    currptr = currpostoptr(fd);
+    if(currptr == NULL)
+      reutnr (-1);
+    
+    //Cuál es el bloque que en el que escribíamos
+    currblock = *currptr;
+    
+    //Si el bloque está en blanco, dale uno
+    if(currblock == 0)
+    {
+     currblock = nextfreeblock();
+      //El bloque encontrdo ponerlo donde
+      
+    }
+  }
 }
 
 vdclose()
