55e82d8819910eb2de18275b09b940e972482b88
DanielBarragan96 <ie702554@iteso.mx>
2018-05-09 12:36:48 -0500

    Add files via upload

---
+++ VDSecLogOperation.c
@@ -0,0 +1,20 @@
+
+
+
+//////////////////////////////////////////////////////
+//													//
+// Lectura y escritura de sectores lógicos			//
+//													//
+//////////////////////////////////////////////////////
+vdreadseclog(int unidad_lógica_o_partición,int sectorlog,char *buffer)
+{
+
+}
+
+vdwriteseclog(int unidad_lógica_o_partición,int sectorlog,char *buffer)
+{
+
+}
+
+
+
---
+++ VDSecLogOperation.h
@@ -0,0 +1,14 @@
+
+
+
+//////////////////////////////////////////////////////
+//													//
+// Lectura y escritura de sectores lógicos			//
+//													//
+//////////////////////////////////////////////////////
+vdreadseclog(int unidad_lógica_o_partición,int sectorlog,char *buffer);
+
+vdwriteseclog(int unidad_lógica_o_partición,int sectorlog,char *buffer);
+
+
+
