881215367e915eb86c5a95dd0b7255ae48fdd5a0
santiago <31044042+SantSou@users.noreply.github.com>
2018-05-10 15:12:05 -0500

    all structures and global variables added

--- DataTypeDefinitions.h
+++ DataTypeDefinitions.h
@@ -1,45 +1,37 @@
-typedef struct{
-	char drive_status;
+struct DATE {
+	int year;
+	int month;
+	int day;
+	int hour;
+	int min;
+	int sec;
+};
+
+struct OPENFILES {
+	int inuse;		// 0 cerrado, 1 abierto
+	unsigned short inode;
+	int currpos;
+	int currbloqueenmemoria;
+	char buffer[1024];
+	unsigned short buffindirect[512]; //		
+}openfiles[24] = {0};
+
+struct PARTITION {
+	char drive_status;	
 	char chs_begin[3];
 	char partition_type;
 	char chs_end[3];
 	int lba;
 	int secs_partition;
-} PARTITION;
+};
 
-// Lo que vamos a escribir en el primer sector del disco
-// Debe medir 512 bytes
-typedef struct{
+struct MBR {
 	char bootstrap_code[446];
-	PARTITION partition[4];
+	struct PARTITION partition[4];
 	short boot_signature;
-} MBR;
-// printf("%d\n",sizeof(struct MBR));
+}mbr;
 
-
-// Debe medir 512 bytes
-typedef struct{
-	char jump[4];			
-	char nombre_particion[8];
-	// Tabla de parámetros del bios
-	// Están los datos sobre el formato de la partición
-	unsigned short sec_inicpart;		// 1 sector 
-	unsigned char sec_res;		// 1 sector reservado para el sector de boot de la partición
-	unsigned char sec_mapa_bits_area_nodos_i;// 1 sector
-	unsigned char sec_mapa_bits_bloques;	// 6 sectores
-	unsigned short sec_tabla_nodos_i;	// 3 sectores
-	unsigned int sec_log_particion;		// 43199 sectores
-	unsigned char sec_x_bloque;			// 2 sectores por bloque
-	unsigned char heads;				// 8 superficies				
-	unsigned char cyls;				// 200 cilindros
-	unsigned char secfis;				// 27 sectores por track
-	char restante[484];	// Código de arranque
-}SECBOOTPART;
-// printf("%d\n",sizeof(struct SECBOOT));
-
-
-// Debe medir 64 bytes, importante es que el tamaño sea potencia de 2
-typedef struct{
+struct INODE {
 	char name[18];
 	unsigned int datetimecreat;	// 32 bits
 	unsigned int datetimemodif;	// 32 bits
@@ -48,8 +40,41 @@ typedef struct{
 	unsigned short gid;		// 16 bits
 	unsigned short perms;	// 16 bits
 	unsigned int size;			// 32 bits
-	unsigned short direct_blocks[10];	// 10 x 16 bits = 20 bytes
+	unsigned short blocks[10];	// 10 x 16 bits = 20 bytes
 	unsigned short indirect;	// 16 bits
 	unsigned short indirect2;	// 16 bits
-}INODE;
+} inode[24] = {0}, emptyinode = {0};
 
+struct SECBOOTPART {
+	char jump[4];
+	char nombre_particion[8];
+	// Tabla de parámetros del bios
+	// Están los datos sobre el formato de la partición
+	unsigned short sec_inicpart;		// 1 sector
+	unsigned char sec_res;		// 1 sector reservado para el sector de boot de la partición
+	unsigned char sec_mapa_bits_area_nodos_i;// 1 sector
+	unsigned char sec_mapa_bits_bloques;	// 6 sectores
+	unsigned short sec_tabla_nodos_i;	// 3 sectores
+	unsigned int sec_log_particion;		// 43199 sectores
+	unsigned char sec_x_bloque;			// 2 sectores por bloque
+	unsigned char heads;				// 8 superficies
+	unsigned char cyls;				// 200 cilindros
+	unsigned char secfis;				// 27 sectores por track
+	char restante[484];	// Código de arranque
+}secboot;
+
+char empty[512] = {0};
+int sl_mb_nodosi;
+int sl_mb_datos;
+int sl_nodosi;
+int sl_datos;
+int sl_heads;
+int sl_cyls;
+int sl_secfis;
+int SFIP;
+int CIP;
+int SIP;
+int TAMBLOQUE;
+char inodesmap[512] = {0};
+char blocksmap[3072] = {0};
+int openfiles_inicializada = 0;
